a034d4dc5b5fd5ac296f5f35b25937329f94620e
Yusuf Russell <yusufamoon@gmail.com>
2026-06-10 21:31:37 -0500

    M5 Task 1

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def create_app() -> Flask:
             title = (request.form.get("title") or "").strip()
             body = (request.form.get("body") or "").strip()
             # TASK 01 will add validation here.
-            app.notes.append({"title": title, "body": body})
+            app.notes.append({"title": title, "body": body, "tags": []})
             return redirect(url_for("home"))
         return render_template("new_note.html")
 
